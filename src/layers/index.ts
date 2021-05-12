c507cc687bafc6df17ec4a40a9c9cb83aa52099f
Caio Henrique <caio_bnh22@hotmail.com>
2021-05-12 20:42:18 -0300

    fix layeers index

---
+++ src/layers/index.ts
@@ -0,0 +1,15 @@
+import Layer1 from './Layer1';
+import Layer2 from './Layer2';
+import Layer3 from './Layer3';
+import Layer4 from './Layer4';
+import Layer5 from './Layer5';
+import Layer6 from './Layer6';
+
+export {
+  Layer1,
+  Layer2,
+  Layer3,
+  Layer4,
+  Layer5,
+  Layer6,
+};
